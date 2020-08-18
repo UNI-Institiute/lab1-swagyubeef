472c56f6fbd9e83127da508f26c175b7f0ae12d5
joerho1@gmail.com <joerho1@gmail.com>
2020-08-17 22:21:06 -0700

    creating starter code for lab1

---
+++ lab1.py
@@ -0,0 +1,11 @@
+#
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name: Jonathan Luu
+# Instructor: Hollister
+# Section: CSC 101_09_2183
+#
+
+print ("Hello, World")
---
+++ vehicle/vehicle_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import vehicle
+
+class VehicleTests(unittest.TestCase):
+    def test_vehicle(self):
+    # Add code here.
+
+
+# Run the unit tests.
+if __name__ == '__main__':
+    unittest.main()
+
